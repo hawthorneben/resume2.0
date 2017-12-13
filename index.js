00bd9b587a4f53caf31930bb325382608f7a8e8d
hawthorneben <hawthorneben@gmail.com>
2017-12-13 14:01:00 -0800

    'test'

--- index.js
+++ index.js
@@ -2,63 +2,55 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
-try 
-{
-    http.createServer(function (request, response) {
-        console.log('request starting...');
+http.createServer(function (request, response) {
+    console.log('request starting...');
 
-        var filePath = '.' + request.url;
-        if (filePath == './')
-            filePath = './index.html';
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
 
-        var extname = path.extname(filePath);
-        var contentType = 'text/html';
-        switch (extname) {
-            case '.js':
-                contentType = 'text/javascript';
-                break;
-            case '.css':
-                contentType = 'text/css';
-                break;
-            case '.json':
-                contentType = 'application/json';
-                break;
-            case '.png':
-                contentType = 'image/png';
-                break;      
-            case '.jpg':
-                contentType = 'image/jpg';
-                break;
-            case '.wav':
-                contentType = 'audio/wav';
-                break;
-        }
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
 
-        fs.readFile(filePath, function(error, content) {
-            if (error) {
-                if(error.code == 'ENOENT'){
-                    fs.readFile('./404.html', function(error, content) {
-                        response.writeHead(200, { 'Content-Type': contentType });
-                        response.end(content, 'utf-8');
-                    });
-                }
-                else {
-                    response.writeHead(500);
-                    response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                    response.end(); 
-                }
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
             }
             else {
-                response.writeHead(200, { 'Content-Type': contentType });
-                response.end(content, 'utf-8');
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
             }
-        });
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
 
-    }).listen(80);
-    console.log('Server running at http://localhost:80/');
-
-}
-catch (er)
-{
-    console.log(er);
-}
+}).listen(80);
+console.log('Server running at http://localhost:80/');
